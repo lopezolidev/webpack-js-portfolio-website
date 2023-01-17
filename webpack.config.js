738c9d3c8beb374e0cd2af0f9552b754eac906e8
rockwatch <rocksistor@gmail.com>
2023-01-17 15:34:05 -0400

    constructed webpack.config.js file and bundled the project for production

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+      // Entry nos permite decir el punto de entrada de nuestra aplicación
+    output: {
+        // Output nos permite decir hacia dónde va enviar lo que va a preparar webpack
+        path: path.resolve(__dirname, 'dist'),
+        // path es donde estará la carpeta donde se guardará los archivos
+        // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
+        filename: 'main.js'
+        // filename le pone el nombre al archivo final
+    },
+    resolve: {
+        extensions: ['.js'],
+        // Aqui van las extensiones que tendremos en nuestro proyecto para webpack las lea
+    }
+}
